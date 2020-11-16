346d7e9465303d4907ca7e9e77c934471a937254
Brennan Abanades Kenyon <56822523+brennanaba@users.noreply.github.com>
2020-11-16 16:44:09 +0000

    Update network.py

--- H3Ranker/network.py
+++ H3Ranker/network.py
@@ -78,5 +78,5 @@ def deep2d_model(lr = 1e-3, blocks = 10):
     phi_end = Activation(activation='softmax')(phi_1)
     
     model = Model(inp, outputs = [dist_end,omega_end,theta_end,phi_end])
-    model.compile(optimizer = Adam(lr), loss = KLDivergence)
+    model.compile(optimizer = Adam(lr), loss = kl_divergence)
     return model
