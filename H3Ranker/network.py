2491f02a3cca9894f11332c06e19e3607866ac98
Brennan Abanades <spamspambrennan@gmail.com>
2020-12-03 11:37:47 +0000

    Making things look prettier

--- H3Ranker/network.py
+++ H3Ranker/network.py
@@ -2,7 +2,8 @@
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.losses import kl_divergence, mse, CategoricalCrossentropy
-from keras.layers import Activation, Add, Conv2D, SpatialDropout2D, Permute, ReLU, Input, BatchNormalization, Layer, Conv1D, SpatialDropout1D, Multiply, Reshape,  GlobalAveragePooling2D
+from keras.layers import Activation, Add, Conv2D, SpatialDropout2D, Permute, ReLU, Input, BatchNormalization, Layer, \
+    Conv1D, SpatialDropout1D, Multiply, Reshape, GlobalAveragePooling2D
 from keras import backend as K
 import numpy as np
 from numba import jit
@@ -13,17 +14,13 @@ current_directory = os.path.dirname(os.path.realpath(__file__))
 # REMEMBER TO pip install . EACH TIME YOU UPDATE
 
 angle_steps = 12.0
-bins = np.arange(-180.,181,angle_steps)
+bins = np.arange(-180., 181, angle_steps)
 mean_angle_bins = (bins[1:] + bins[:-1]) / 2
 classes = len(bins)
 
-dist_bins = np.linspace(3,17,classes - 2)
-dist_bins = np.append(dist_bins,20)
+dist_bins = np.linspace(3, 17, classes - 2)
+dist_bins = np.append(dist_bins, 20)
 mean_dist_bins = (dist_bins[1:] + dist_bins[:-1]) / 2
-
-def JensenShannonDivergence(y_true, y_pred):
-    y_mean = (y_true + y_pred)/2
-    return (kl_divergence(y_true,y_mean) + kl_divergence(y_pred, y_mean))/2
 
 
 @jit
@@ -32,76 +29,79 @@ def encode(x, classes):
     one_hot[x] = 1
     return one_hot
 
+
 @jit
-def one_hot(num_list, classes = 21):
+def one_hot(num_list, classes=21):
     end_shape = (len(num_list), classes)
     finish = np.zeros(end_shape)
     for i in range(end_shape[0]):
         finish[i] = encode(num_list[i], classes)
     return finish
 
-class Expand_Dimensions(Layer):
+
+class ExpandDimensions(Layer):
     def __init__(self, **kwargs):
         super(Expand_Dimensions, self).__init__(**kwargs)
 
     def call(self, x):
         a = K.expand_dims(x, axis=-2)
-        b = K.permute_dimensions(a, (0,2,1,3))
+        b = K.permute_dimensions(a, (0, 2, 1, 3))
         return a + b
 
-def deep2d_model(lr = 1e-2, blocks = 20, blocks_1d = 5):
+
+def deep2d_model(lr=1e-2, blocks=20, blocks_1d=5):
     inp = Input(shape=(None, 21))
-    mix1 = Conv1D(64, kernel_size= 17, strides = 1, padding= "same", name = "1Dconv_1", trainable = True)(inp)
+    mix1 = Conv1D(64, kernel_size=17, strides=1, padding="same", name="1Dconv_1", trainable=True)(inp)
     block_start_1d = SpatialDropout1D(0.5)(mix1)
-    
+
     for i in range(blocks_1d):
-        block_conv1_1d = Conv1D(64, kernel_size= 17, strides = 1, padding= "same", trainable = True)(block_start_1d)
+        block_conv1_1d = Conv1D(64, kernel_size=17, strides=1, padding="same", trainable=True)(block_start_1d)
         block_act_1d = ReLU()(block_conv1_1d)
         block_drop_1d = SpatialDropout1D(0.5)(block_act_1d)
-        block_conv2_1d = Conv1D(64, kernel_size= 17, strides = 1, padding= "same", trainable = True)(block_drop_1d)
-        block_norm_1d = BatchNormalization(scale = True, trainable = True)(block_conv2_1d)
-        block_start_1d = Add()([block_start_1d,block_norm_1d])
+        block_conv2_1d = Conv1D(64, kernel_size=17, strides=1, padding="same", trainable=True)(block_drop_1d)
+        block_norm_1d = BatchNormalization(scale=True, trainable=True)(block_conv2_1d)
+        block_start_1d = Add()([block_start_1d, block_norm_1d])
 
-    
     activate_1d = ReLU()(block_start_1d)
-    block_start = Expand_Dimensions()(activate_1d)
+    block_start = ExpandDimensions()(activate_1d)
     for i in range(blocks):
-        block_conv1 = Conv2D(64, kernel_size= 5, strides = 1, padding= "same", trainable = True, dilation_rate = 2**(i%6))(block_start)
+        block_conv1 = Conv2D(64, kernel_size=5, strides=1, padding="same", trainable=True, dilation_rate=2 ** (i % 6))(
+            block_start)
         block_act = ReLU()(block_conv1)
         block_drop = SpatialDropout2D(0.5)(block_act)
-        block_conv2 = Conv2D(64, kernel_size= 5, strides = 1, padding= "same", trainable = True, dilation_rate = 2**(i%6))(block_drop)
-        block_norm = BatchNormalization(scale = True, trainable = True)(block_conv2)
-        
+        block_conv2 = Conv2D(64, kernel_size=5, strides=1, padding="same", trainable=True, dilation_rate=2 ** (i % 6))(
+            block_drop)
+        block_norm = BatchNormalization(scale=True, trainable=True)(block_conv2)
+
         attend_pool = GlobalAveragePooling2D()(block_norm)
-        attend_reshape = Reshape((1,1,64))(attend_pool)
-        attend_conv1 = Conv2D(16, activation= "relu", kernel_size = 1, trainable = True)(attend_reshape)
-        attend_out = Conv2D(64, activation= "sigmoid", kernel_size = 1, trainable = True)(attend_conv1)
+        attend_reshape = Reshape((1, 1, 64))(attend_pool)
+        attend_conv1 = Conv2D(16, activation="relu", kernel_size=1, trainable=True)(attend_reshape)
+        attend_out = Conv2D(64, activation="sigmoid", kernel_size=1, trainable=True)(attend_conv1)
         attend_end = Multiply()([attend_out, block_norm])
-        
-        block_start = Add()([block_start,attend_end])
-        
+
+        block_start = Add()([block_start, attend_end])
+
     block_end = block_start
     activate = ReLU()(block_end)
     drop = SpatialDropout2D(0.5)(activate)
-    
-    
+
     # This should be number of classes, defined by the number of bins
-    dist_1 = Conv2D(classes, kernel_size= 3, strides = 1, padding= "same", trainable = True)(drop)
-    dist_2 = Permute(dims=(2,1,3))(dist_1)
-    dist_symmetry = Add()([dist_1,dist_2])
-    dist_end = Activation(activation='softmax', name= "distance_out")(dist_symmetry)
-    
-    omega_1 = Conv2D(classes, kernel_size= 3, strides = 1, padding= "same", trainable = True)(drop)
-    omega_2 = Permute(dims=(2,1,3))(omega_1)
-    omega_symmetry = Add()([omega_1,omega_2])
-    omega_end = Activation(activation='softmax', name= "omega_out")(omega_symmetry)
-    
-    theta_1 = Conv2D(classes, kernel_size= 3, strides = 1, padding= "same", trainable = True)(drop)
-    theta_end = Activation(activation='softmax', name = "theta_out")(theta_1)
-    
-    phi_1 = Conv2D(classes, kernel_size= 3, strides = 1, padding= "same", trainable = True)(drop)
-    phi_end = Activation(activation='softmax', name = "phi_out")(phi_1)
-    
-    model = Model(inp, outputs = [dist_end,omega_end,theta_end,phi_end])
-    model.compile(optimizer = Adam(lr), loss = CategoricalCrossentropy(), loss_weights = [1/6,1/6,2/6,2/6])
+    dist_1 = Conv2D(classes, kernel_size=3, strides=1, padding="same", trainable=True)(drop)
+    dist_2 = Permute(dims=(2, 1, 3))(dist_1)
+    dist_symmetry = Add()([dist_1, dist_2])
+    dist_end = Activation(activation='softmax', name="distance_out")(dist_symmetry)
+
+    omega_1 = Conv2D(classes, kernel_size=3, strides=1, padding="same", trainable=True)(drop)
+    omega_2 = Permute(dims=(2, 1, 3))(omega_1)
+    omega_symmetry = Add()([omega_1, omega_2])
+    omega_end = Activation(activation='softmax', name="omega_out")(omega_symmetry)
+
+    theta_1 = Conv2D(classes, kernel_size=3, strides=1, padding="same", trainable=True)(drop)
+    theta_end = Activation(activation='softmax', name="theta_out")(theta_1)
+
+    phi_1 = Conv2D(classes, kernel_size=3, strides=1, padding="same", trainable=True)(drop)
+    phi_end = Activation(activation='softmax', name="phi_out")(phi_1)
+
+    model = Model(inp, outputs=[dist_end, omega_end, theta_end, phi_end])
+    model.compile(optimizer=Adam(lr), loss=CategoricalCrossentropy(), loss_weights=[1 / 6, 1 / 6, 2 / 6, 2 / 6])
     return model
--- H3Ranker/train.py
+++ H3Ranker/train.py
@@ -3,7 +3,7 @@ import pandas as pd
 from numba import jit
 from H3Ranker.network import dist_bins, mean_dist_bins, bins, mean_angle_bins, deep2d_model, one_hot
 from keras.utils import Sequence
-from keras.callbacks import EarlyStopping,  ModelCheckpoint, CSVLogger, ReduceLROnPlateau
+from keras.callbacks import EarlyStopping, ModelCheckpoint, CSVLogger, ReduceLROnPlateau
 import os
 import sys
 
@@ -11,11 +11,13 @@ current_directory = os.path.dirname(os.path.realpath(__file__))
 val_table = pd.read_csv(os.path.join(current_directory, "validation_data.csv"))
 data = pd.read_csv(os.path.join(current_directory, "train_data.csv"))
 
-dict_ = {'A': '0', 'C': '1', 'D': '2', 'E': '3', 'F': '4', 'G': '5', 'H': '6', 'I': '7', 'K': '8', 'L': '9', 'M': '10', 'N': '11', 'P': '12', 'Q': '13', 'R': '14', 'S': '15', 'T': '16', 'V': '17', 'W': '18', 'Y': '19', '-': '20'}
+dict_ = {'A': '0', 'C': '1', 'D': '2', 'E': '3', 'F': '4', 'G': '5', 'H': '6', 'I': '7', 'K': '8', 'L': '9', 'M': '10',
+         'N': '11', 'P': '12', 'Q': '13', 'R': '14', 'S': '15', 'T': '16', 'V': '17', 'W': '18', 'Y': '19', '-': '20'}
 classes = len(bins)
 
+
 @jit
-def gauss_encode_distance(measured, std = (dist_bins[3] - dist_bins[2])):
+def gauss_encode_distance(measured, std=(dist_bins[3] - dist_bins[2])):
     answer = np.zeros(classes)
     if measured < 0:
         answer[0] = 1
@@ -24,90 +26,99 @@ def gauss_encode_distance(measured, std = (dist_bins[3] - dist_bins[2])):
         answer[-1] = 1
         return answer
     else:
-        answer[1:-1] = (1/std*np.sqrt(2*np.pi))*np.exp((-((mean_dist_bins - measured)/std)**2)/2)
-        return answer/np.sum(answer)
-    
+        answer[1:-1] = (1 / std * np.sqrt(2 * np.pi)) * np.exp((-((mean_dist_bins - measured) / std) ** 2) / 2)
+        return answer / np.sum(answer)
+
+
 @jit
-def gauss_encode_angles(measured, std = (bins[3] - bins[2])):
+def gauss_encode_angles(measured, std=(bins[3] - bins[2])):
     answer = np.zeros(classes)
     if measured < -180:
         answer[0] = 1
         return answer
     else:
-        answer[1:] = (1/std*np.sqrt(2*np.pi))*np.exp((-(np.abs(measured%360 - mean_angle_bins%360)/std)**2)/2)
-        return answer/np.sum(answer)
+        answer[1:] = (1 / std * np.sqrt(2 * np.pi)) * np.exp(
+            (-(np.abs(measured % 360 - mean_angle_bins % 360) / std) ** 2) / 2)
+        return answer / np.sum(answer)
+
 
 @jit
-def encode_distances(matrix, std = (dist_bins[3] - dist_bins[2])):
+def encode_distances(matrix, std=(dist_bins[3] - dist_bins[2])):
     """ Goes throught each distance measurement and encodes it into different bins as a gaussian.
     """
     end_shape = (matrix.shape[0], matrix.shape[1], classes)
     finish = np.zeros(end_shape)
     for i in range(end_shape[0]):
         for j in range(end_shape[1]):
-            finish[i,j] = gauss_encode_distance(matrix[i,j], std = std)
+            finish[i, j] = gauss_encode_distance(matrix[i, j], std=std)
     return finish
 
+
 @jit
-def encode_angles(matrix, std = (bins[3] - bins[2])):
+def encode_angles(matrix, std=(bins[3] - bins[2])):
     """ Goes throught each angle measurement and encodes it into different bins as a gaussian.
     """
     end_shape = (matrix.shape[0], matrix.shape[1], classes)
     finish = np.zeros(end_shape)
     for i in range(end_shape[0]):
         for j in range(end_shape[1]):
-            finish[i,j] = gauss_encode_angles(matrix[i,j], std = std)
+            finish[i, j] = gauss_encode_angles(matrix[i, j], std=std)
     return finish
+
 
 def sort_distance_into_bins(x, dist_bins):
     x[x < dist_bins[0]] = dist_bins[0]
     x[x == float("Nan")] = -1
     return np.digitize(x, dist_bins)
 
+
 @jit
 def sort_angles_into_bins(x, bins):
-    x = (x+180)%360 - 180
+    x = (x + 180) % 360 - 180
     x = np.where(np.isnan(x), -1e5, x)
     return np.digitize(x, bins)
 
+
 @jit
-def encode_sorted(sortd, classes = 31):
+def encode_sorted(sortd, classes=31):
     xsize = sortd.shape[0]
     ysize = sortd.shape[1]
-    result = np.zeros((xsize,ysize,classes))
+    result = np.zeros((xsize, ysize, classes))
     for i in range(xsize):
         for j in range(ysize):
-            result[i,j,sortd[i,j]] = 1
+            result[i, j, sortd[i, j]] = 1
     return result
 
 
 class DataLoader(Sequence):
-    'Loads data for Keras'
-    def __init__(self, data,shuffle = True, batch_size = 8):
-        'Initialization'
+    """Loads data for Keras"""
+
+    def __init__(self, data, shuffle=True, batch_size=8):
+        """Initialization"""
         self.data = data
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.on_epoch_end()
 
     def __len__(self):
-        'Denotes the number of batches per epoch'
-        return len(self.data)//self.batch_size
+        """Denotes the number of batches per epoch"""
+        return len(self.data) // self.batch_size
 
     def __getitem__(self, index):
-        'Generate one batch of data'
+        """Generate one batch of data"""
 
         # Generate data
-        X, y = self.__data_generation(self.data[index*self.batch_size:self.batch_size*(index+1)].reset_index(drop=True))
+        x, y = self.__data_generation(
+            self.data[index * self.batch_size:self.batch_size * (index + 1)].reset_index(drop=True))
 
-        return X, y
+        return x, y
 
     def on_epoch_end(self):
-        if self.shuffle == True:
-            self.data = self.data.sample(frac = 1).reset_index(drop=True)
+        if self.shuffle:
+            self.data = self.data.sample(frac=1).reset_index(drop=True)
 
     def __data_generation(self, data):
-        'Generates data containing batch_size samples'
+        """Generates data containing batch_size samples"""
         batch_tfirst = []
         batch_tsecond = []
         batch_tsecond1 = []
@@ -115,11 +126,19 @@ class DataLoader(Sequence):
         batch_tlabels = []
         # Generate data
         for i in range(len(data)):
-            pair = np.load(os.path.join(current_directory, "../../data/"+data.ID[i]+".npy"))
-            first = encode_sorted(sort_distance_into_bins(pair[0] + np.random.normal(0, (dist_bins[3] - dist_bins[2]), pair[0].shape), dist_bins)) #encode_distances(pair[0])
-            second = encode_sorted(sort_angles_into_bins(pair[1] + np.random.normal(0, (bins[3] - bins[2]), pair[1].shape), bins)) #encode_angles(pair[1])
-            second1 = encode_sorted(sort_angles_into_bins(pair[2] + np.random.normal(0, (bins[3] - bins[2]), pair[2].shape), bins)) #encode_angles(pair[2])
-            second2 = encode_sorted(sort_angles_into_bins(pair[3] + np.random.normal(0, (bins[3] - bins[2]), pair[3].shape), bins)) #encode_angles(pair[3])
+            pair = np.load(os.path.join(current_directory, "../../data/" + data.ID[i] + ".npy"))
+            first = encode_sorted(
+                sort_distance_into_bins(pair[0] + np.random.normal(0, (dist_bins[3] - dist_bins[2]), pair[0].shape),
+                                        dist_bins))  # encode_distances(pair[0])
+            second = encode_sorted(
+                sort_angles_into_bins(pair[1] + np.random.normal(0, (bins[3] - bins[2]), pair[1].shape),
+                                      bins))  # encode_angles(pair[1])
+            second1 = encode_sorted(
+                sort_angles_into_bins(pair[2] + np.random.normal(0, (bins[3] - bins[2]), pair[2].shape),
+                                      bins))  # encode_angles(pair[2])
+            second2 = encode_sorted(
+                sort_angles_into_bins(pair[3] + np.random.normal(0, (bins[3] - bins[2]), pair[3].shape),
+                                      bins))  # encode_angles(pair[3])
             seq = data.Sequence[i]
             first_in = one_hot(np.array([int(dict_[x]) for x in seq]))
             batch_tlabels.append(first_in)
@@ -127,22 +146,22 @@ class DataLoader(Sequence):
             batch_tsecond.append(second)
             batch_tsecond1.append(second1)
             batch_tsecond2.append(second2)
-            
-        return  np.array(batch_tlabels), [np.array(batch_tfirst), np.array(batch_tsecond),np.array(batch_tsecond1),np.array(batch_tsecond2)]
+
+        return np.array(batch_tlabels), [np.array(batch_tfirst), np.array(batch_tsecond), np.array(batch_tsecond1),
+                                         np.array(batch_tsecond2)]
 
 
 if __name__ == "__main__":
     latest = os.path.join(current_directory, "models", str(sys.argv[1]))
     print("Saving model to: " + latest)
-    model = deep2d_model(lr = 0.005)
-    
-    
+    model = deep2d_model(lr=0.005)
+
     training_generator = DataLoader(data, batch_size=4)
     validation_generator = DataLoader(val_table, batch_size=4)
-    
-    es =  EarlyStopping(patience= 20, restore_best_weights= True)
-    check = ModelCheckpoint(filepath=latest, save_best_only= True, save_weights_only= True)
+
+    es = EarlyStopping(patience=20, restore_best_weights=True)
+    check = ModelCheckpoint(filepath=latest, save_best_only=True, save_weights_only=True)
     log = CSVLogger("results.csv")
-    lr_reduce = ReduceLROnPlateau(cooldown = 5)
-    model.fit(training_generator,validation_data=validation_generator, epochs= 500, callbacks=[check, log, lr_reduce, es])
-            
+    lr_reduce = ReduceLROnPlateau(cooldown=5)
+    model.fit(training_generator, validation_data=validation_generator, epochs=500,
+              callbacks=[check, log, lr_reduce, es])
